539b0a63f3d84e2cccf242029839b0771ef67ce6
fshangala <fshangala@gmail.com>
2024-06-16 18:16:45 +0200

    getting meta advertisers

--- ScraperSDK/winninghunt.py
+++ ScraperSDK/winninghunt.py
@@ -1,5 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+
 from enum import Enum
 from bs4 import BeautifulSoup
 import time
@@ -10,6 +14,17 @@ import queue, threading
 
 class ActiveStatus(Enum):
   ALL="all"
+  
+class element_exists(object):
+  def __init__(self,css_selector:str):
+    self.css_selector=css_selector
+  
+  def __call__(self,driver):
+    b = driver.execute_script(f'return document.querySelector("{self.css_selector}");')
+    if b:
+      return b
+    else:
+      return False
 
 class WinningHunt:
   def __init__(self):
@@ -20,16 +35,6 @@ class WinningHunt:
     options.add_argument('--disable-gpu')
 
     self.driver = webdriver.Chrome(options=options)
-    key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNzEzMzA0ODAwLAogICJleHAiOiAxODcxMDcxMjAwCn0.wzm6GJDkA5uIKsaUcZPHG2mm-lCDIHCaFX1GiWlXDtQ"
-    url="http://copiwin.com:8000"
-    self.supabase = create_client(url,key)
-    
-  def adDetails(self,ad)->list:
-    """
-    Get ad details
-    """
-    a0000 = list(list(list(list(ad.children)[0].children)[0].children)[0])[0]
-    return a0000
 
   def getAdsFromPageSource(self)->list:
     """
@@ -47,23 +52,35 @@ class WinningHunt:
     ads = list(list(adsContainer.children)[0].children)
     return ads
   
-  def fetch(self,session,active_status=ActiveStatus.ALL):
+  def fetch(self):
     """
     Fetch ads page source
     """
-    self.driver.get(f"https://web.facebook.com/ads/library/?active_status={active_status}&ad_type=all&country=ZM&q=boots&sort_data[direction]=desc&sort_data[mode]=relevancy_monthly_grouped&search_type=keyword_unordered&media_type=all")
+    self.driver.get(f"https://winninghunter.com/pages")
+  
+  def login(self):
+    email = self.driver.find_element(By.NAME, "Email")
+    email.send_keys("malaky31@hotmail.fr")
+    password = self.driver.find_element(By.NAME, "Password")
+    password.send_keys("12345678")
+    button = self.driver.find_element(By.CSS_SELECTOR, 'button.login')
+    button.click()
+  
+  def moveToMetaAdvertisers(self):
+    self.driver.get(f"https://winninghunter.com/pages")
+    data = None
+    if "Login" in self.driver.title:
+      self.login()
+      WebDriverWait(self.driver,10).until(expected_conditions.title_contains("Dashboard"))
+      data = self.moveToMetaAdvertisers()
+    elif "Pages" in self.driver.title:
+      WebDriverWait(self.driver,20).until(element_exists("#store-table .dt-center a"))
+      data = self.driver.execute_script("var table = document.querySelector(\"#store-table\");var data=[];for(var i=1;i<table.rows.length;i++){var row={};for(var j=0;j<table.rows[i].cells.length;j++){row[table.rows[0].cells[j].innerText]=table.rows[i].cells[j].innerText;data.push(row);}};return data;")
+      
+    return data
     
-    ads = self.getAdsFromPageSource()
-    text_ads = list()
-    for ad in ads:
-      details = list()
-      for detail in self.adDetails(ad):
-        details.append(detail.text)
-      text_ads.append(details)
-    ads_data={"ads":text_ads}
-    result = self.supabase.table("facebookads").insert({
-      "data":json.dumps(ads_data),
-      "session":session,
-    }).execute()
-    print(result)
-    return result
+# w = WinningHunt()
+# data = w.moveToMetaAdvertisers()
+# print(data)
+# print(type(data))
+# print(dir(data))
