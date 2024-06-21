a7d62637508f5d8696c091d446159957bea60518
fshangala <fshangala@gmail.com>
2024-06-21 21:15:28 +0200

    added save-ad api endpoint

--- scraper/serializers.py
+++ scraper/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from scraper.models import SavedAd
 
 class UserSerializer(serializers.ModelSerializer):
   class Meta:
     model: User
+    fields: "__all__"
+
+class SavedAdSerializer(serializers.ModelSerializer):
+  class Meta:
+    model: SavedAd
     fields: "__all__"
