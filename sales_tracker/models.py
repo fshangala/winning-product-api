d2e22f03ae056d4c5bbca55a9bf3ed2d473953f3
fshangala <fshangala@gmail.com>
2024-07-09 19:12:27 +0000

    good staff

--- sales_tracker/models.py
+++ sales_tracker/models.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import User
 class Store(models.Model):
   user=models.ForeignKey(to=User,on_delete=models.CASCADE,related_name='stores')
   title=models.CharField(max_length=200)
-  url=models.URLField(unique=True)
-  hostname=models.CharField(max_length=200,null=True)
+  url=models.URLField()
+  hostname=models.CharField(max_length=200)
 
   def __str__(self) -> str:
     return self.title
