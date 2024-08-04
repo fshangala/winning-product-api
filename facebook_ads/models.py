0426fdc98e3fa252973a0abc25a16a130e76783b
fshangala <fshangala@gmail.com>
2024-08-04 07:16:30 +0200

    created models and serializers

--- facebook_ads/models.py
+++ facebook_ads/models.py
@@ -1,5 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class FacebookPage(models.Model):
+  page_id=models.BigIntegerField(unique=True)
+  page_url=models.URLField()
+  name=models.CharField(max_length=200)
+  likes=models.IntegerField()
+  profile_picture_url=models.URLField()
+  ig_username=models.CharField(max_length=200,null=True)
+  ig_followers=models.IntegerField(default=0)
+  
+facebook_ad_display_format_choices=(
+  ('image','Image'),
+  ('video','Video'),
+)
 class FacebookAd(models.Model):
-  ad_archive_id=models.CharField(max_length=200)
+  page=models.ForeignKey(to=FacebookPage,on_delete=models.CASCADE,related_name='ads')
+  ad_archive_id=models.BigIntegerField(unique=True)
+  ad_creative_id=models.BigIntegerField(unique=True)
+  display_format=models.CharField(max_length=200,choices=facebook_ad_display_format_choices)
+  link_url=models.URLField()
+  image=models.URLField()
+  video=models.URLField()
+  video_preview=models.URLField()
+  creation_time=models.DateTimeField()
+  start_date=models.DateTimeField()
+  end_date=models.DateTimeField()
+  body_html=models.TextField()
+  caption=models.CharField(max_length=200)
+  cta_text=models.CharField(max_length=200)
+  
--- facebook_ads/serializers.py
+++ facebook_ads/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
 from ApiSDK.meta_ad_library import MetaAdLibrary
+from facebook_ads.models import facebook_ad_display_format_choices
 
 class FacebookAdSearchSerializer(serializers.Serializer):
-  search_term=serializers.CharField(required=True)
+  search_term=serializers.CharField()
   country_code=serializers.CharField(required=False,default="US",initial="US")
   
   def retrieve(self):
@@ -10,3 +11,30 @@ class FacebookAdSearchSerializer(serializers.Serializer):
     ads = meta.searchAds(search_term=self.validated_data['search_term'],country_code=self.validated_data['country_code'])
     
     return ads
+
+class FacebookPageSerializer(serializers.Serializer):
+  id=serializers.IntegerField(read_only=True)
+  page_id=serializers.IntegerField()
+  page_url=serializers.URLField()
+  name=serializers.CharField()
+  likes=serializers.IntegerField(default=0,initial=0)
+  profile_picture_url=serializers.URLField()
+  ig_username=serializers.CharField(required=False)
+  ig_followers=serializers.IntegerField(required=False,default=0,initial=0)
+
+class FacebookAdSerializer(serializers.Serializer):
+  id=serializers.IntegerField(read_only=True)
+  page=FacebookPageSerializer(read_only=True,many=False)
+  ad_archive_id=serializers.IntegerField()
+  ad_creative_id=serializers.IntegerField()
+  display_format=serializers.ChoiceField(choices=facebook_ad_display_format_choices)
+  link_url=serializers.URLField()
+  image=serializers.URLField()
+  video=serializers.URLField()
+  video_preview=serializers.URLField()
+  creation_time=serializers.DateTimeField()
+  start_date=serializers.DateTimeField()
+  end_date=serializers.DateTimeField()
+  body_html=serializers.CharField()
+  caption=serializers.CharField()
+  cta_text=serializers.CharField()
