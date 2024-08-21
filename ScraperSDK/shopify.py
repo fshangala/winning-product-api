4dd765e7117f62ee846f96fae8a32b18ed902f6b
fshangala <fshangala@gmail.com>
2024-08-21 12:16:41 +0200

    using extracted url

--- ScraperSDK/shopify.py
+++ ScraperSDK/shopify.py
@@ -6,7 +6,8 @@ import re
 
 class ShopifyProduct:
   def __init__(self,url):
-    response = requests.get(url+".json")
+    extractedUrl = url.split("?")[0]
+    response = requests.get(extractedUrl+".json")
     data = response.json()
     try:
       self.data=data["product"]
