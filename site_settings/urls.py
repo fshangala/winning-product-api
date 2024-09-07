979168e1a8045a04ebd626c7427092f6598507ac
fshangala <fshangala@gmail.com>
2024-09-07 16:27:17 +0200

    save and update auto_load_facebook_ads

--- site_settings/urls.py
+++ site_settings/urls.py
@@ -2,4 +2,4 @@ from rest_framework.routers import DefaultRouter
 from site_settings.views import SiteSettingsViewSet
 
 site_settings_router=DefaultRouter()
-site_settings_router.register(r"site-settings/",viewset=SiteSettingsViewSet,basename='site-settings')
+site_settings_router.register(r"api-admin/site-settings",viewset=SiteSettingsViewSet,basename='site-settings')
